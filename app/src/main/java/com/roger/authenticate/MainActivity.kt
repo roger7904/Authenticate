97f3710f8df88fdbf6438107e8542a3748d6fd6a
roger <roger@25sprout.com>
2022-04-24 00:07:50 +0800

    feat:implement facebook sign in

--- app/src/main/java/com/roger/authenticate/MainActivity.kt
+++ app/src/main/java/com/roger/authenticate/MainActivity.kt
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -19,6 +26,7 @@ import com.roger.authenticate.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var auth: FirebaseAuth = Firebase.auth
+    private val callbackManager: CallbackManager = CallbackManager.Factory.create()
     private var mGoogleSignInClient: GoogleSignInClient? = null
     private val googleSignInLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
@@ -34,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initGoogleSignInClient()
+        initFacebookCallbackManager()
 
         binding.run {
             clGoogle.setOnClickListener {
@@ -41,6 +50,57 @@ class MainActivity : AppCompatActivity() {
                     googleSignInLauncher.launch(mGoogleSignInClient?.signInIntent)
                 }
             }
+
+            clFacebook.setOnClickListener {
+                LoginManager.getInstance().logInWithReadPermissions(
+                    this@MainActivity,
+                    callbackManager,
+                    listOf("public_profile", "email")
+                )
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if (auth.currentUser != null){
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun initFacebookCallbackManager() {
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(result: LoginResult?) {
+                    if (result?.accessToken != null) {
+                        handleFacebookSignInResult(result.accessToken)
+                    }
+                }
+
+                override fun onCancel() {}
+                override fun onError(error: FacebookException) {}
+            })
+    }
+
+    private fun handleFacebookSignInResult(token: AccessToken) {
+        try {
+            val credential = FacebookAuthProvider.getCredential(token.token)
+            auth.signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, ProfileActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    }
+                }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
